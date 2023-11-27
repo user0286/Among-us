047ff08c8126b4718dc77381ee4cdcc7c9090931
Qiwiss <braveking1818@gmail.com>
2023-11-27 19:29:08 +0700

    Make Layout

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Phaser from 'phaser'
+
+import AmongUsScene from './scene/AmongUsScene'
+
+
+const config = {
+	type: Phaser.AUTO,
+	parent: 'app',
+	width: 1920,
+	height: 1080,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 },
+		},
+	},
+	scene: [AmongUsScene],
+}
+
+export default new Phaser.Game(config)
